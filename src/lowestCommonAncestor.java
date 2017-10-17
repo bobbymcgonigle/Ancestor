9f22635d0b0fd7ae3195ab62017b1059ed26010c
Rory Saunders <rosaunde@tcd.ie>
2017-10-17 14:56:49 +0100

    Added the least common ancestor function and added a test file

--- src/lowestCommonAncestor.java
+++ src/lowestCommonAncestor.java
@@ -39,6 +39,26 @@ class treeNode{
 	int getData() {
 		return data;
 	}
+	//found at: https://www.programcreek.com/2014/07/leetcode-lowest-common-ancestor-of-a-binary-tree-java/
+	
+	public treeNode lowestCommonAncestor(treeNode root, treeNode p, treeNode q) {
+	    if(root==null)
+	        return null;
+	 
+	    if(root==p || root==q)
+	        return root;
+	 
+	    treeNode l = lowestCommonAncestor(root.left, p, q);
+	    treeNode r = lowestCommonAncestor(root.right, p, q);
+	 
+	    if(l!=null&&r!=null){
+	        return root;
+	    }else if(l==null&&r==null){
+	        return null;
+	    }else{
+	        return l==null?r:l;
+	    }
+	}
 }
 
 class tree{
