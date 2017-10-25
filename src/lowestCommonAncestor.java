4a58ae37d671ffc327e10da524521db43f0f3044
Rory Saunders <rosaunde@tcd.ie>
2017-10-25 21:34:28 +0100

    Finished coding a possible solution, it's long and ugly but I'm going to test it now.

--- src/lowestCommonAncestor.java
+++ src/lowestCommonAncestor.java
@@ -1,8 +1,8 @@
- import java.util.Scanner;
- import java.util.Arrays;
- import java.util.ArrayList;
- 
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.ArrayList;
 
+// tree solution code
 /*
 class treeNode{
 	treeNode left, right;
@@ -65,122 +65,174 @@ class treeNode{
 	}
 }
 */
- class DAGnode{
-	 ArrayList<DAGnode> parents;
-	 ArrayList<DAGnode> children;
-	 int data;
-	 String color;
-	 int count;
-	 
-	 public DAGnode() {
-		 data = 0;
-		 color = "white";
-		 count = 0;
-		 parents = null;
-		 children = null;
-	 }
-	 
-	 public DAGnode(int input) {
-		 data = input;
-		 color = "white";
-		 count = 0;
-		 parents = null;
-		 children = null;
-	 }
-	 
-	 void addChild(DAGnode child) {
-		 children.add(child);
-		 child.parents.add(this);
-	 }
-	 
-	 void addColor(String input) {
-		 color = input;
-	 }
-	 
-	 void increment() {
-		 count++;
-	 }
-	 
-	 public DAGnode lowestCommonAncestor(DAGnode p, DAGnode q) {
-		    if(p==q) {
-		    	return p;
-		    }
-		    
-		    if(p.parents == null && q.parents == null) {
-		    	return null;
-		    }
-		    
-		    if(p.parents == null || q.parents == null) {
-		    	return p;
-		    }
-		    
-		    String pColor = "red";
-		    colorAllAncestors(p, pColor);
-		    
-		    String qColor = "blue";
-		    colorAllCommonAncestors(q, qColor, pColor);
-		    
-	 
- }
-	 void colorAllAncestors(DAGnode theNode, String nodeColor) {
-		 if(theNode != null && nodeColor != null) {
-			 while (theNode.parents != null) {
-				 theNode.color = nodeColor;
-				 for(int i = 0; i < theNode.parents.size(); i++) {
-					 colorAllAncestors(theNode.parents.get(i), nodeColor);
-				 }
-			 }
-		 }
-		 
-	 }
-	 
-	 void colorAllCommonAncestors(DAGnode theNode, String newColor, String oldColor) {
-		 if(theNode != null && newColor != null && oldColor != null) {
-			 while (theNode.parents != null) {
-				 if(theNode.color == oldColor) {
-					 theNode.color = newColor;
-					 for(int i = 0; i < theNode.parents.size(); i++) {
-						 colorAllCommonAncestors(theNode.parents.get(i), newColor, oldColor);
-					 }
-				 }
-				 
-			 }
-		 }
-	 }
- 
- }
+class DAGnode {
+	ArrayList<DAGnode> parents;
+	ArrayList<DAGnode> children;
+	int data;
+	String color;
+	int count;
+
+	public DAGnode() {		//make an empty node, initialize parents and children
+		data = 0;
+		color = "white";
+		count = 0;
+		parents = new ArrayList();
+		children = new ArrayList();
+	}
+
+	public DAGnode(int input) {		//make a node with data, initialize parents and children
+		data = input;
+		color = "white";
+		count = 0;
+		parents = new ArrayList();
+		children = new ArrayList();
+	}
+
+	void addChild(DAGnode child) {
+		children.add(child);
+		child.parents.add(this);
+	}
+
+	void addColor(String input) {
+		color = input;
+	}
+
+	void increment() {
+		count++;
+	}
+	
+	//this method takes an inputed node, and gives all of its ancestors a color, recursively
+	
+	void colorAllAncestors(DAGnode theNode, String nodeColor) {
+		if (nodeColor != null) {
+			while (theNode != null) {
+				theNode.color = nodeColor;
+				if (!theNode.parents.isEmpty()) {
+					for (int i = 0; i < theNode.parents.size(); i++) {
+						colorAllAncestors(theNode.parents.get(i), nodeColor);
+					}
+				}
+			}
+		}
+
+	}
+	
+	//once one node has all of its ancestors colored, this goes and colors all the common ancestors a different color
+	
+	void colorAllCommonAncestors(DAGnode theNode, String newColor, String oldColor) {
+		if (newColor != null && oldColor != null) {
+			while (theNode != null) {
+				if (theNode.color == oldColor) {
+					theNode.color = newColor;
+					if (!theNode.parents.isEmpty()) {
+						for (int i = 0; i < theNode.parents.size(); i++) {
+							colorAllCommonAncestors(theNode.parents.get(i), newColor, oldColor);
+						}
+					}
+				}
+			}
+		}
+	}
+	
+	//this method the parents of each common ancestor by one, leaving the least common ancestor(s) colored and with value 0
+	
+	void counting(DAGnode theNode, String theColor) {
+		if (theNode != null && theColor != null) {
+			if (!theNode.parents.isEmpty() && theNode.color == theColor) {
+				for (int i = 0; i < theNode.parents.size(); i++) {
+					theNode.parents.get(i).increment();
+				}
+			}
+			for (int j = 0; j < theNode.children.size(); j++) {
+				counting(theNode.children.get(j), theColor);
+			}
+		}
+
+	}
+	
+	//this recursively travels through the list appending all found LCAs to an arraylist
+	void findAnswers(DAGnode head, ArrayList<DAGnode> answers, String theColor){
+		if(head != null && answers != null && theColor != null) {
+			if(head.color == theColor && head.count == 0) {
+				answers.add(head);
+			}
+			
+			if(!head.children.isEmpty()) {
+				for(int i = 0; i < head.children.size(); i++){
+					findAnswers(head.children.get(i), answers, theColor);
+				}
+			}
+		}
+	}
+
+	public ArrayList<DAGnode> lowestCommonAncestor(DAGnode head, DAGnode p, DAGnode q) {
+		ArrayList<DAGnode> lowestCommonAncestors = new ArrayList();
+		if (p == q) {
+			lowestCommonAncestors.add(p);
+			return lowestCommonAncestors;
+		}
+
+		if (p.parents.isEmpty() && q.parents.isEmpty()) {
+			return null;
+		}
+
+		if (p.parents.isEmpty() || q.parents.isEmpty()) {
+			lowestCommonAncestors.add(p);
+			return lowestCommonAncestors;
+		}
+
+		String pColor = "red";
+		colorAllAncestors(p, pColor);
+
+		String qColor = "blue";
+		colorAllCommonAncestors(q, qColor, pColor);
+
+		counting(head, qColor);
+		
+		findAnswers(head, lowestCommonAncestors, qColor);
+		
+		return lowestCommonAncestors;
+	}
+
+}
+
+//Dear person grading this, really sorry this is so long, decided to just take a glance at the problem and code it all myself.
+//There is probably a lot of poor efficiency and needless code in here; I'm out of practice.
+
 public class lowestCommonAncestor {
 
 	public static void main(String[] args) {
+		
+		DAGnode head = new DAGnode
+		
+		
+		
+		
+		
+		
+		
+		//testing the tree solution
 		/*
-		treeNode root = new treeNode(7);
-		root.left = new treeNode(9);
-		
-		treeNode Dave = new treeNode(5);
-		root.right = Dave;
-		
-		root.left.left = new treeNode(4);
-		root.left.right = new treeNode(11);
-		root.left.left.left = new treeNode(17);
-		root.left.left.right = new treeNode(27);
-		root.left.right.left = new treeNode(1);
-		root.left.right.right = new treeNode(91);
-		root.right.left = new treeNode(13);
-		root.right.left.left = new treeNode(72);
-		
-		treeNode Jack = new treeNode(14);
-		root.right.left.right = Jack;
-		
-		treeNode Fred = new treeNode(46);
-		root.right.right = Fred;
-		
-		root.right.right.left = new treeNode(12);
-		root.right.right.right = new treeNode(3);
-		
-		int result = root.lowestCommonAncestor(root, Jack, Fred).getData();
-		System.out.print(result);
-		*/
+		 * treeNode root = new treeNode(7); root.left = new treeNode(9);
+		 * 
+		 * treeNode Dave = new treeNode(5); root.right = Dave;
+		 * 
+		 * root.left.left = new treeNode(4); root.left.right = new treeNode(11);
+		 * root.left.left.left = new treeNode(17); root.left.left.right = new
+		 * treeNode(27); root.left.right.left = new treeNode(1); root.left.right.right =
+		 * new treeNode(91); root.right.left = new treeNode(13); root.right.left.left =
+		 * new treeNode(72);
+		 * 
+		 * treeNode Jack = new treeNode(14); root.right.left.right = Jack;
+		 * 
+		 * treeNode Fred = new treeNode(46); root.right.right = Fred;
+		 * 
+		 * root.right.right.left = new treeNode(12); root.right.right.right = new
+		 * treeNode(3);
+		 * 
+		 * int result = root.lowestCommonAncestor(root, Jack, Fred).getData();
+		 * System.out.print(result);
+		 */
 	}
-	
-}
 
+}
